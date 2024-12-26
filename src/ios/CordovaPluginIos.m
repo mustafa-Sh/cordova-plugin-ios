6ebd4d9c01757ada763d8d1a44bb814ef8643511
PeOmniBk <PeOmniBk@path-solutions.com>
2024-12-26 14:38:35 +0200

    write code To detect a jailbroken device, check for the presence of Cydia

--- src/ios/CordovaPluginIos.m
+++ src/ios/CordovaPluginIos.m
@@ -2,11 +2,7 @@
 
 #import <Cordova/CDV.h>
 
-@interface CordovaPluginIos : CDVPlugin {
-  // Member variables go here.
-}
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@interface CordovaPluginIos : CDVPlugin
 @end
 
 @implementation CordovaPluginIos
@@ -14,26 +10,54 @@
 - (void)coolMethod:(CDVInvokedUrlCommand*)command
 {
     NSString* echo = [command.arguments objectAtIndex:0];
-    if (echo != nil && [echo length] > 0) {
-        // Show a toast-like alert
-        UIAlertController* alert = [UIAlertController alertControllerWithTitle:nil
-                                                                       message:echo
-                                                                preferredStyle:UIAlertControllerStyleAlert];
-        [self.viewController presentViewController:alert animated:YES completion:^{
-            // Dismiss the alert after 2 seconds
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-                [alert dismissViewControllerAnimated:YES completion:nil];
-            });
-        }];
-
-        // Send success result back to JavaScript
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    BOOL isJailbroken = [self isDeviceJailbroken];
+    
+    NSString* responseMessage;
+    if (isJailbroken) {
+        responseMessage = [NSString stringWithFormat:@"%@ - Jailbroken Device Detected!", echo];
     } else {
-        // Send error result back to JavaScript
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid argument"];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        responseMessage = [NSString stringWithFormat:@"%@ - Device is Secure.", echo];
     }
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:nil
+                                                                   message:responseMessage
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    [self.viewController presentViewController:alert animated:YES completion:^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+            [alert dismissViewControllerAnimated:YES completion:nil];
+        });
+    }];
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:responseMessage];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (BOOL)isDeviceJailbroken {
+    // Check for the existence of Cydia's path
+    if ([[NSFileManager defaultManager] fileExistsAtPath:@"/Applications/Cydia.app"]) {
+        return YES;
+    }
+    // Check for other jailbreak files
+    NSArray *jailbreakPaths = @[
+        @"/Library/MobileSubstrate/MobileSubstrate.dylib",
+        @"/bin/bash",
+        @"/usr/sbin/sshd",
+        @"/etc/apt"
+    ];
+    for (NSString *path in jailbreakPaths) {
+        if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+            return YES;
+        }
+    }
+    // Check if the app can write outside its sandbox
+    NSString *testPath = @"/private/jailbreak_test.txt";
+    NSError *error = nil;
+    [@"Jailbreak Test" writeToFile:testPath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    if (error == nil) {
+        [[NSFileManager defaultManager] removeItemAtPath:testPath error:nil];
+        return YES;
+    }
+    return NO;
 }
 
 @end
